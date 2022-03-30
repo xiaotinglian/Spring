5a378b04d0dd3a7132464728816206dfc4c1da73
Xiaoting Lian <lian1995226@gmail.com>
2022-03-30 09:40:31 -0400

    1

--- src/main/java/org/genspark/App.java
+++ src/main/java/org/genspark/App.java
@@ -7,8 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args ) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Student obj =(Student)context.getBean("student");
