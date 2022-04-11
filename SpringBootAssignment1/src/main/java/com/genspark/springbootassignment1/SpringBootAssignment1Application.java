2b60bed7ae7492ed0866254b34744380552a97b9
Xiaoting Lian <lian1995226@gmail.com>
2022-04-11 12:11:48 -0400

    springboot assignment added

--- SpringBootAssignment1/src/main/java/com/genspark/springbootassignment1/SpringBootAssignment1Application.java
+++ SpringBootAssignment1/src/main/java/com/genspark/springbootassignment1/SpringBootAssignment1Application.java
@@ -1,4 +1,4 @@
-package com.genspark.springbootassignment1;
+package com.genspark.SpringBootAssignment1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootAssignment1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootAssignment1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssignment1Application.class, args);
+	}
 
 }
