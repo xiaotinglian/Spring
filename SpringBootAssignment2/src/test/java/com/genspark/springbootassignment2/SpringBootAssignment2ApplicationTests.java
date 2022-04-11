f5e9e5b7b3fc93b0ae78c159c1c24ded99518a4f
Xiaoting Lian <lian1995226@gmail.com>
2022-04-11 14:00:43 -0400

    SpringBootAssignment2 Springboot with database

---
+++ SpringBootAssignment2/src/test/java/com/genspark/springbootassignment2/SpringBootAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.springbootassignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
